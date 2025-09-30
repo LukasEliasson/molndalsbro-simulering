0d3e31d00115b208abf876f4c117a88712df7fd3
knotthult <07eliand@gafe.molndal.se>
2025-09-30 15:08:28 +0200

    Experimental environment for pathfinding testing + New pathfinding model

--- scripts/Person.gd
+++ scripts/Person.gd
@@ -3,22 +3,29 @@ extends CharacterBody2D
 @export var speed: float = 100.0
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
+var previous_distance: float = 0.0
+
 func _ready() -> void:
 	navigation_agent.velocity_computed.connect(_on_velocity_computed)
 
 func set_target(target: Vector2) -> void:
 	navigation_agent.set_target_position(target)
+	previous_distance = global_position.distance_to(target)
 
 func _physics_process(delta: float) -> void:
-	# If agent has a path, move toward the next point
 	if navigation_agent.is_navigation_finished():
-		# We reached our goal, so remove the Person
 		queue_free()
 		return
 	
 	var next_position = navigation_agent.get_next_path_position()
 	var desired_velocity = (next_position - global_position).normalized() * speed
 	navigation_agent.set_velocity(desired_velocity)
+
+	
+	var next_pos = navigation_agent.get_next_path_position()
+	var direction = (next_pos - global_position).normalized()
+	if velocity.dot(direction) < 0:
+		navigation_agent.set_target_position(navigation_agent.get_target_position())
 
 func _on_velocity_computed(safe_velocity: Vector2) -> void:
 	velocity = safe_velocity
--- scripts/Station.gd
+++ scripts/Station.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var person_scene = preload("res://scenes/Person.tscn")
-@export var spawn_interval: float = 1.0
+@export var spawn_interval: float = 0.4
 
 func _ready() -> void:
 	spawn_person()
